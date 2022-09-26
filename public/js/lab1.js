a7c512bb5b1bfac6d429017005e5926eec450943
X00173557 Daniel Mc Carthy <X00173557@ta.it-tallaght.ie>
2022-09-26 11:45:36 +0100

    lab commit

---
+++ public/js/lab1.js
@@ -0,0 +1,15 @@
+
+function add(x, y) {
+    answer = x + y;
+    return answer
+}
+
+function CheckNums(num1, num2) {
+    if (num1 > num2){
+        console.log("Num 1 is greater.");
+    }else if (num2 > num1) {
+        console.log("Num 2 is greater.")
+    }else{
+        console.log("Both are equal.")
+    }
+}
